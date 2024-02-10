2a9d5b0e091db796f4c193baecee191952fd231d
Winycios <winycios.nascimento@sptech.school>
2024-02-10 19:18:34 -0300

    Slides automaticos

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -14,10 +14,10 @@ var swiper = new Swiper(".slide-content", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-    // autoplay: {
-    //     delay: 3000,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
     breakpoints: {
         0: {
             slidesPerView: 1
